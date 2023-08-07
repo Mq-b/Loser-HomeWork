585483ae51f6ed9fb258e425ae469423aa2c74e5
DarkSharpness <2040703891@qq.com>
2023-08-07 12:04:58 +0800

    Simply the answer

--- src/群友提交/第七题/DarkSharpness.cpp
+++ src/群友提交/第七题/DarkSharpness.cpp
@@ -12,65 +12,20 @@ void f2() {
 }
 
 
-/* A recommended version! */
-namespace dark {
-
-/**
- * @brief Actually you'd better use override
- * to explicitly demostrate the type of the function......
- * Anyway, the behaviors are all the same :(
- * 
-*/
-struct error : std::exception {
-    std::string data;
-    error(std::string_view __str) : data(__str) { puts("error()"); }
-    ~error() override { puts("~error()"); }
-    const char *what() const noexcept override { return data.data(); }
-};
-
-/* Too lazy to complete...... */
-struct unknown_error { std::string data; };
-
-}
-
-/**
- * @brief A better version, I think......
- * Actually, I don't know why throwing a pointer......
- * 
-*/
-void f1() {
-    throw dark::error("new Exception异常....");
-}
-
-void f3() { throw dark::unknown_error {.data = "What the f**k is that?"}; }
-
-void f4() { throw "?"; }
-
 signed main(){
     try {
-        try {
-            // f1(); // what i recommend
-            f2(); // std
-            // f3(); // test for unknown error
-            f4();
-        } catch(std::exception *__exception) {
-            /* NEVER catch the derived class unless necessary. */
-            puts(__exception->what());
-            delete __exception;
-        } catch(std::exception &__exception) {
-            /* Use reference type to catch the virtual class. */
-            puts(__exception.what());
-            puts("This version is what I recommend.");
-        }  catch(...) {
-            puts("Unknown error!");
-            throw ; /* It will throw the error caught. */
-        }
-    } catch (dark::unknown_error &__exception) {
-        std::cout << __exception.data << '\n';
-    } catch (...) {
-        puts("Program panic!");
-        return 1; /* Abnormal exit. */
+        f2(); // std
+    } catch(std::exception *__exception) {
+        /* NEVER catch the derived class unless necessary. */
+        puts(__exception->what());
+        delete __exception;
+    } catch(std::exception &__exception) {
+        /* Use reference type to catch the virtual class. */
+        puts(__exception.what());
+        puts("This version is what I recommend.");
+    }  catch(...) {
+        puts("Unknown error!");
+        throw ; /* It will throw the error caught. */
     }
-
     return 0; /* Normal case. */
 }
