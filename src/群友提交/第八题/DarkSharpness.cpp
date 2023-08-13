a79e8c711c92b78fe0e746287809a1243d3fbfe7
DarkSharpness <2040703891@qq.com>
2023-08-13 15:44:27 +0800

    Test

---
+++ src/群友提交/第八题/DarkSharpness.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+template <class Ty, size_t size>
+struct array {
+    Ty* begin() { return arr; };
+    Ty* end() { return arr + size; };
+    Ty arr[size];
+};
+
+
+/* Size of array cannot be 0! */
+template <class T,class ...U>
+requires (std::is_same_v <T,U> && ...)
+array(T , U...) -> array <T, sizeof...(U) + 1>;
+
+int main() {
+    ::array arr = {1,2,3,4,5};
+    for (const auto& i: arr) {
+        std::cout << i << ' ';
+    }
+}
