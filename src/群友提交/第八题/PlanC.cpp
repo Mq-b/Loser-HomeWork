752fbce40ed65ca2fb0e0f2da7889bc0feff4962
baiyi-Jiang <pathfinding_knight@163.com>
2023-08-14 16:42:32 +0800

    1.提交第八题

---
+++ src/群友提交/第八题/PlanC.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+template<class Ty, size_t size>
+struct array {
+    Ty* begin() { return arr; };
+    Ty* end() { return arr + size; };
+    Ty arr[size];
+};
+
+template<typename Ty, typename ...Args>
+array(Ty, Args...) -> array<std::enable_if_t<(std::is_same_v<Ty, Args>&&...), Ty>, sizeof...(Args) + 1>;
+
+int main() {
+    ::array arr{1, 2, 3, 4, 5};
+    for (const auto& i : arr) {
+        std::cout << i << ' ';
+    }
+}
