86cdda3efd0fbf5dc66425c6bd702addf4afb82e
Sugar0612 <287276293@qq.com>
2023-08-16 23:44:53 +0800

    feat: Commit Wgtteol homework_9.

---
+++ src/群友提交/第九题/Wgtteol.cpp
@@ -0,0 +1,26 @@
+﻿#include<iostream>
+
+template<class T>
+struct X {
+    void f()const { std::cout << "X\n"; }
+};
+
+void f() { std::cout << "全局\n"; }
+
+template<class T>
+struct Y : X<T> {
+    void t()const {
+        this->f();
+        // 因为 Y 继承 X， 又因为 使用了 this (X<T>*)，所以是一个 实参依赖查找，所以查找的是 X<T> 实例 的f();
+    }
+    void t2()const {
+        f();
+        // 没用使用 this，也没有使用 ::， 所以是 无限定的名字查找，所以查找的是全局的 f();
+    }
+};
+
+int main() {
+    Y<void>y;
+    y.t();
+    y.t2();
+}
