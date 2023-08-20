2f4bdb3fce715210af0cc96abd290f8318981181
heroboy <ywqywq@yeah.net>
2023-08-20 19:30:46 +0800

    使用预处理生成代码

--- src/群友提交/第10题/494199.cpp
+++ src/群友提交/第10题/494199.cpp
@@ -38,44 +38,69 @@ namespace {
 	{
 		static constexpr size_t value = is_excat_size<T, N> ? N : _size<T, N - 1>::value;
 	};
+	template<typename T>
+	consteval size_t size()
+	{
+		return _size<T, 100>::value;
+	}
 }
 
+#define LOOP0(body,sep)
+#define LOOP1(body,sep) body(0)
+#define LOOP2(body,sep) LOOP1(body,sep) sep() body(1)
+#define LOOP3(body,sep) LOOP2(body,sep) sep() body(2)
+#define LOOP4(body,sep) LOOP3(body,sep) sep() body(3)
 
+//LOOP(2) EXPAND TO: body(0) sep() body(1)
+#define LOOP(n,body,sep) LOOP ## n(body,sep)
 
-
-template<typename T>
-consteval size_t size()
-{
-	return _size<T,100>::value;
-}
 
 template<class T>
 void for_each_member(const T& obj, auto&& f)
 {
-	if constexpr (size<T>() == 4)
-	{
-		const auto& [m0, m1, m2, m3] = obj;
-		f(m0), f(m1), f(m2), f(m3);
+#define EMPTY()
+#define COMMA() ,
+
+//m0
+#define MAKE_M_VAR(n) m##n
+//f(m0)
+#define MAKE_F_CALL(n) f(m##n)
+//const auto& [m0, m1, m2, m3] = obj;
+#define MAKE_UNPACK(n) const auto& [ LOOP(n,MAKE_M_VAR,COMMA) ] = obj
+//f(m0), f(m1), f(m2), f(m3);
+#define MAKE_CALL(n) LOOP(n, MAKE_F_CALL, COMMA);
+
+//MAKE_ALL_CHECK(4) EXPAND TO:
+//if constexpr (size<T>() == 4)
+//{
+//	const auto& [m0, m1, m2, m3] = obj;
+//	f(m0), f(m1), f(m2), f(m3);
+//}
+#define MAKE_ALL_CHECK(n) \
+	if constexpr (size<T>() == n)\
+	{\
+		MAKE_UNPACK(n);\
+		MAKE_CALL(n);\
 	}
-	if constexpr (size<T>() == 3)
-	{
-		const auto& [m0, m1, m2] = obj;
-		f(m0), f(m1), f(m2);
-	}
-	if constexpr (size<T>() == 2)
-	{
-		const auto& [m0, m1] = obj;
-		f(m0), f(m1);
-	}
-	if constexpr (size<T>() == 1)
-	{
-		const auto& [m0] = obj;
-		f(m0);
-	}
+
+	MAKE_ALL_CHECK(4);
+	MAKE_ALL_CHECK(3);
+	MAKE_ALL_CHECK(2);
+	MAKE_ALL_CHECK(1);
+
+#undef MAKE_ALL_CHECK
+#undef MAKE_CALL
+#undef MAKE_UNPACK
+#undef MAKE_F_CALL
+#undef MAKE_M_VAR
+#undef COMMA
+#undef EMPTY
 }
 
 
+
 int main() {
+
 	struct X { std::string s{ " " }; }x;
 	struct Y { double a{}, b{}, c{}, d{}; }y;
 
