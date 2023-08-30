ea6476bf53b18250c57e0ee8b5f58c18e0489a0d
rsp4jack <creepercdn@outlook.com>
2023-08-30 21:30:40 +0800

    fix tabs to 4space

--- src/群友提交/第07题/saidfljwnzjasf.cpp
+++ src/群友提交/第07题/saidfljwnzjasf.cpp
@@ -3,13 +3,13 @@
 #include <exception>
 
 struct MyException :std::exception {
-	const char* data{};
-	MyException(const char* s) :data(s) { puts("MyException()"); }
-	~MyException() { puts("~MyException()"); }
-	const char* what()const noexcept { return data; }
+    const char* data{};
+    MyException(const char* s) :data(s) { puts("MyException()"); }
+    ~MyException() { puts("~MyException()"); }
+    const char* what()const noexcept { return data; }
 };
 void f2() {
-	throw new MyException("new Exception异常....");
+    throw new MyException("new Exception异常....");
 }
 int main(){
     try
--- src/群友提交/第08题/Wgtteol.cpp
+++ src/群友提交/第08题/Wgtteol.cpp
@@ -2,9 +2,9 @@
 #include <array>
 template<class Ty, size_t size>
 struct array {
-	Ty* begin() { return arr; }
-	Ty* end() { return arr + size; }
-	Ty arr[size];
+    Ty* begin() { return arr; }
+    Ty* end() { return arr + size; }
+    Ty arr[size];
 };
 
 /*
@@ -14,9 +14,9 @@ template<class T, class... U> array(T, U...) -> array<T, 1 + sizeof...(U)>;
 
 int main()
 {
-	::array arr{1, 2, 3, 4, 5};
-	for (const auto& i : arr) {
-		std::cout << i << ' ';
-	}
-	return 0;
+    ::array arr{1, 2, 3, 4, 5};
+    for (const auto& i : arr) {
+        std::cout << i << ' ';
+    }
+    return 0;
 }
