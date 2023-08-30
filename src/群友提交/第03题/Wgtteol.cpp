de6b5a2391c0d52f60b947cfcd4224b88555e757
rsp4jack <creepercdn@outlook.com>
2023-08-30 21:30:40 +0800

    fix tabs to 4space

--- src/群友提交/第03题/Wgtteol.cpp
+++ src/群友提交/第03题/Wgtteol.cpp
@@ -2,36 +2,36 @@
 #include <iostream>
 
 struct Frac {
-	int a, b;
+    int a, b;
 };
 
 template<>
 struct std::formatter<Frac>: std::formatter<char>  {
-	/*
-	auto parse(const auto& ctx) const { // fist call
-		return ctx.begin();
-	}
-	*/
+    /*
+    auto parse(const auto& ctx) const { // fist call
+        return ctx.begin();
+    }
+    */
 
-	auto format(const Frac& frac, auto& ctx) const { // second call
-		return std::format_to(ctx.out(), "{}/{}", frac.a, frac.b);
-	}
+    auto format(const Frac& frac, auto& ctx) const { // second call
+        return std::format_to(ctx.out(), "{}/{}", frac.a, frac.b);
+    }
 };
 
 auto operator ""_f(const char* str_c, size_t n)
 {
-	return [=](auto&& ...elems)
-	{
-		return std::vformat(str_c, std::make_format_args(elems...));
-	};
+    return [=](auto&& ...elems)
+    {
+        return std::vformat(str_c, std::make_format_args(elems...));
+    };
 }
 
 void print(std::string_view fmt, auto&&...arg) {
-	std::cerr << std::vformat(fmt, std::make_format_args(arg...)) << std::endl;
+    std::cerr << std::vformat(fmt, std::make_format_args(arg...)) << std::endl;
 }
 
 int main() 
 {
-	Frac frac{ 1, 10 };
-	print("{}", frac);
+    Frac frac{ 1, 10 };
+    print("{}", frac);
 }
--- src/群友提交/第07题/DarkSharpness.cpp
+++ src/群友提交/第07题/DarkSharpness.cpp
@@ -2,13 +2,13 @@
 
 
 struct MyException : std::exception {
-	const char* data{};
-	MyException(const char* s) :data(s) { puts("MyException()"); }
-	~MyException() { puts("~MyException()"); }
-	const char* what() const noexcept { return data; }
+    const char* data{};
+    MyException(const char* s) :data(s) { puts("MyException()"); }
+    ~MyException() { puts("~MyException()"); }
+    const char* what() const noexcept { return data; }
 };
 void f2() {
-	throw new MyException("new Exception异常....");
+    throw new MyException("new Exception异常....");
 }
 
 
