9d1438ae20f07bcd1a895bbe0ba17fd9c31e4f23
AzrBrk <pbrink51@gmail.com>
2023-12-24 20:53:54 +0800

    Update akchilov.cpp
    第一版写的有点啰嗦，改一下

--- src/群友提交/第10题/akchilov.cpp
+++ src/群友提交/第10题/akchilov.cpp
@@ -2,18 +2,13 @@
 #include<iostream>
 #include<string>
 
-
+//任意类
 struct init
 {
     template<class T> operator T();
 };
 
-template<class T, class ...Args> struct check_constructible
-{
-    static constexpr bool value = std::is_constructible_v<T, Args...>;
-};
-
-
+//从一个用一个递减的类型列表尝试能不能std::is_constructible_v
 template<bool, class T, class construct_list, size_t cnt> struct count_size
 {
     static constexpr size_t value = cnt;
@@ -24,49 +19,41 @@ struct count_size<false, T, construct_list<First, rest...>, cnt>
 {
     static constexpr size_t value =
         count_size<
-        check_constructible<T, First, rest...>::value,
+        std::is_constructible_v<T, First, rest...>,
         T,
-        construct_list<rest...>, 
-        (check_constructible<T, First, rest...>::value ? cnt : cnt - 1)>
+        construct_list<rest...>,
+       (std::is_constructible_v<T, First, rest...>? cnt : cnt - 1)>
         ::value;
 };
-
-template<class T, class cl> struct count_size<false, T, cl, 0>
-{
-    static_assert(true, "构造不了！！！");
-    static constexpr size_t value = 0;
-};
-
 template<class ...> struct tl {};
-template<class T> using count_member = count_size<false, T, tl<init, init, init, init>, 4>;
 
 template<class T> constexpr size_t size()
 {
-    return count_member<std::decay_t<T>>::value;
+    return count_size<false, std::decay_t<T>, tl<init, init, init, init>, 4>::value;
 }
 
-template<class T, class F> constexpr void for_each_member (T&& i, F&& f)
+template<class T, class F> constexpr void for_each_member(T&& i, F&& f)
 {
     if constexpr (size<T>() == 4u)
     {
-        auto && [a, b, c, d] = i;
+        auto&& [a, b, c, d] = i;
         f(a), f(b), f(c), f(d);
-    } 
+    }
     if constexpr (size<T>() == 3u)
     {
-        auto && [a, b, c] = i;
+        auto&& [a, b, c] = i;
         f(a), f(b), f(c);
-    } 
+    }
     if constexpr (size<T>() == 2u)
     {
-        auto && [a, b] = i;
+        auto&& [a, b] = i;
         f(a), f(b);
-    } 
+    }
     if constexpr (size<T>() == 1u)
     {
-        auto &&[a] = i;
+        auto&& [a] = i;
         f(a);
-    } 
+    }
 }
 
 
