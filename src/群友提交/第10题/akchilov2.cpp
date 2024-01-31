362954f1bd7d3b2158fffccde65871058befd07b
AzrBrk <pbrink51@gmail.com>
2024-02-01 01:12:30 +0800

    Update akchilov2.cpp
    https://gcc.godbolt.org/z/Y1e193Mcr 同步更改编译时计算版

--- src/群友提交/第10题/akchilov2.cpp
+++ src/群友提交/第10题/akchilov2.cpp
@@ -63,7 +63,7 @@ namespace possibilities
 
     template<class ...> struct tl {};
 
-    template<class T> using count_size_type = count_size<false, std::decay_t<T>, tl<inits, inits, inits, inits, inits, inits, inits, inits, inits, inits>, 10>;
+    template<class T> using count_size_type = count_size<false, std::decay_t<T>, tl<inits, inits, inits, inits, inits, inits, inits, inits, inits, inits, inits, inits, inits, inits, inits>, 15>;
     template<class T> constexpr std::size_t size() {
         return count_size_type<T>::value;
     }
@@ -106,88 +106,85 @@ namespace possibilities
 }
 
 namespace offset_pointer {
-    //编译时计算
-    //offset_ptr模拟成员在结构体中的分布， advance：跨越一个指定的size， forward_offset:寻找下一个成员的首地址
-    template<std::size_t pack_size, std::size_t layer_offset, std::size_t offset> struct align_offset_ptr
-    {
-        static_assert(pack_size <= 8, "An alignment size exceeds 8 is not tested!");
-        static constexpr std::size_t layer = layer_offset;//alignment layer as base
-        static constexpr std::size_t value = offset;//当前层偏移指针
-        static constexpr std::size_t half_pack = pack_size / 2;//层的半对齐
-        //工具：移动到下一内存对齐的层， 层偏移指针置零
-        template<std::size_t N> using advance_layer = align_offset_ptr<pack_size, layer + N, 0>;
-        //当forward offset + 当前层偏移超出或等于内存对齐的值
-        //假如forward offset+value==pack_size， 或则当前层偏移为0时，成员会存在当前层， 移动 size/pack_size 层，
-        //否则当size%pack_size == 0, size/pack_size + 1层，因为它会被放在下一层
-        //否则当size%pack_size != 0, size/pack_size + 2层(暂时没见过这种情况，数组可能会出现，但是这个类不让数组推导)
-        //因为不会出现一个成员的一部分和另外一个成员在同一层的情况
-        template<bool offset_overflow_pack, std::size_t forward_offset> struct advance_impl
-        {
-            static constexpr std::size_t forward_base = forward_offset / pack_size;
-            static constexpr bool has_rest = static_cast<bool>(forward_offset % pack_size);
-            static constexpr std::size_t forward_base_fixed = has_rest ? forward_base + 1 : forward_base;
-            using type = advance_layer<(((forward_offset + value) == pack_size) ? 0 : (value == 0 ? 0 : 1)) + forward_base_fixed>;
-        };
-        //当forward offset + 当前层偏移小于内存对齐的值，位移不超过当前层
-        //但是当forward offset超过半对齐，成员就会存储在后一半层， 否则直接移动forward offset
-        template<std::size_t forward_offset> struct advance_impl<false, forward_offset>
-        {
-            using type_impl = align_offset_ptr<pack_size, layer,
-                (((value + forward_offset) > half_pack) && (forward_offset >= half_pack)) ?
-                pack_size :
-                value + forward_offset>;
-            using type = std::conditional_t<type_impl::value == 8, advance_layer<1>, type_impl>;
-        };
-        //工具：使指针越过一个成员的位置
-        template<std::size_t forward_offset> using advance = typename advance_impl<((value + forward_offset) >= pack_size), forward_offset>::type;
-        //探索指针，用于寻找下一个成员的首地址，只在get中使用
-        //枚举了给定的指针是否超过半对齐，内存对齐的情况
-        template<std::size_t forward_size> struct forward_impl
-        {
-            using type = std::conditional_t<(forward_size >= pack_size),
-                advance_layer<(value == 0) ? 0 : 1>, align_offset_ptr<pack_size, layer,
-                ((forward_size >= half_pack) ? ((value == 0) ? 0 : half_pack) : value) >>;
-        };
-        template<std::size_t forward_size> using seek = typename forward_impl<forward_size>::type;
-        //基于当前的层偏移，使用探索指针寻找给定的类型的首地址
-        template<class T, class type> static type& get(T* ptr)
-        {
-            static_assert(!std::is_array_v<type>, "calculating size of an array is not tested!");
-            return *(reinterpret_cast<type*>(
-                reinterpret_cast<unsigned char*>(ptr) +
-                (
-                    seek<sizeof(type)>::layer * pack_size +
-                    seek<sizeof(type)>::value
-                    )));
-        }
-    };
-    //递归迭代，实现类似std::get的方法
-    template<std::size_t N, class offset_pointer, class TL> struct offset_iter {};
-    template<std::size_t N, class offset_pointer, template<class ...> class TL, class first, class ...rest>
-    struct offset_iter<N, offset_pointer, TL<first, rest...>>
-    {
-        using recurse_type = offset_iter<N - 1, typename offset_pointer::template advance<sizeof(first)>, TL<rest...>>;
-        using iter_type = typename recurse_type::iter_type;
-        using type = typename recurse_type::type;
+    template<typename structT, typename T>
+ T& read_from_align_offset(structT* base_ptr, const std::size_t offset)
+ {
+     return *(reinterpret_cast<T*>(reinterpret_cast<unsigned char*>(base_ptr) + offset));
+ }
+ //编译时计算
+ //offset_ptr模拟成员在结构体中的分布， advance：跨越一个指定的size， forward_offset:寻找下一个成员的首地址
+ template<std::size_t pack_size, std::size_t layer_offset, std::size_t offset> struct align_offset_ptr
+ {
+     static_assert(pack_size <= 8, "An alignment size exceeds 8 is not tested!");
+     static constexpr std::size_t layer = layer_offset;//alignment layer as base
+     static constexpr std::size_t value = offset;//当前层偏移指针
+     template<std::size_t lay> struct advance_layer
+     {
+         using type = align_offset_ptr<pack_size, layer + lay, 0>;
+     };
+     template<bool can_be_placed, std::size_t seek_offset> struct seek_impl
+     {
+         using type = align_offset_ptr<pack_size, layer, value>;
+     };
+     template<std::size_t seek_offset> struct seek_impl<false, seek_offset>
+     {
+         using type = typename std::conditional_t<
+             (value + seek_offset > pack_size),
+             typename advance_layer<1>::type,
+             align_offset_ptr<pack_size, layer_offset, value + 1>
+             >::template seek<seek_offset>;
+     };
+     template<std::size_t seek_offset> using seek = typename seek_impl<value % seek_offset == 0,seek_offset>::type;
 
-    };
-    template<class offset_pointer, template<class...> class TL, class first, class ...rest>
-    struct offset_iter<0, offset_pointer, TL<first, rest...>>
-    {
-        using type = first;
-        using iter_type = offset_pointer;
-    };
-    //封装成函数
-    template<std::size_t I, class CTL, class T> auto& get_member(T* x)
-    {
-        using types_iter = offset_iter<I, align_offset_ptr<alignof(T), 0, 0>, CTL>;
-        return types_iter::iter_type::template get<T, typename types_iter::type>(x);
-    }
+     template<std::size_t advance_offset> struct advance_impl
+     {
+         using type = std::conditional_t<(value + advance_offset < pack_size),
+             align_offset_ptr<pack_size, layer, value + advance_offset>,
+             std::conditional_t<
+             static_cast<bool>(advance_offset% pack_size),
+             typename advance_layer<advance_offset / pack_size + 1>::type,
+             typename advance_layer<advance_offset / pack_size>::type>
+         >;
+     };
+     template<std::size_t advance_offset> using advance = typename advance_impl<advance_offset>::type;
+     template<class structT, class T> 
+     static T& get(structT* base_ptr)
+     {
+         using seek_ptr = seek<sizeof(T)>;
+         return read_from_align_offset<structT, T>(base_ptr, seek_ptr::layer * pack_size + seek_ptr::value);
+     }
+ };
+ //递归迭代，实现类似std::get的方法
+ template<std::size_t N, class offset_pointer, class TL> struct offset_iter {};
+ template<std::size_t N, class offset_pointer, template<class ...> class TL, class first, class ...rest>
+ struct offset_iter<N, offset_pointer, TL<first, rest...>>
+ {
+     using recurse_type = offset_iter<N - 1, 
+         typename offset_pointer
+         ::template seek<sizeof(first)>
+         ::template advance<sizeof(first)>, TL<rest...>>;
+     using iter_type = typename recurse_type::iter_type;
+     using type = typename recurse_type::type;
+
+ };
+ template<class offset_pointer, template<class...> class TL, class first, class ...rest>
+ struct offset_iter<0, offset_pointer, TL<first, rest...>>
+ {
+     using type = first;
+     using iter_type = offset_pointer;
+ };
+ //封装成函数
+ template<std::size_t I, class CTL, class T> auto& get_member(T* x)
+ {
+     using types_iter = offset_iter<I, align_offset_ptr<alignof(T), 0, 0>, CTL>;
+
+     return types_iter::iter_type::template get<T, typename types_iter::type>(x);
+ }
 }
 
 using namespace possibilities;
 using namespace offset_pointer;
-using possible = tl<int, std::string, double, char, int>;
+using possible = tl<int, std::string, double, char, int, short>;
 
 template<class T, class F, size_t ...I> constexpr void for_each_member_impl(T&& x, F&& f, std::index_sequence<I...>)
 {
@@ -200,18 +197,22 @@ template<class T, class F> constexpr void for_each_member(T&& x, F&& f)
 }
 
 int main() {
-    struct X
-    {
-        char cc{ 'k' };
-        int a{ 10 };
-        char c{ 'l' };
-        char e{ '3' };
-        char h{ 'o' };
-        int ac{ 19 };
-        double b{ 2.33 };
-        double bbg{ 4.55 };
-        char j{ 'j' };
-    } x{};    struct Y { double a{}, b{}, c{}, d{}; }y;
+   struct X
+{
+    char aaa{ 'A' };
+    int aaaak{ 100 };
+    std::string str{ "hello X!" };
+    char cc{ 'k' };
+    int a{ 10 };
+    char c{ 'l' };
+    short r{ 123 };
+    char e{ 'u' };
+    short rr{ 889 };
+    int ac{ 19 };
+    double b{ 2.33 };
+    std::string str2{ "hello X again!" };
+    double bbg{ 4.55 };
+} x{};    struct Y { double a{}, b{}, c{}, d{}; }y;
     std::cout << size<X>() << '\n';
     std::cout << size<Y>() << '\n';
 
