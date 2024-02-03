1ecf9f0c8cbd712d19a78c3f760d3d0ea09b43da
Suzukaze <1027743497@qq.com>
2024-02-03 17:40:25 +0800

    涼風青葉修改第3题 提交第4-8题

---
+++ src/群友提交/第07题/涼風青葉.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<exception>
+
+struct MyException :std::exception {
+    const char *data{};
+    MyException(const char *s) :data(s) { puts("MyException()"); }
+    ~MyException() { puts("~MyException()"); }
+    const char *what()const noexcept { return data; }
+};
+void f2() {
+    throw new MyException("new Exception异常....");
+}
+int main() {
+    try {
+        f2();
+    } catch (MyException *e) {
+        std::cout << e->what() << std::endl;
+        delete e;
+    }
+}
