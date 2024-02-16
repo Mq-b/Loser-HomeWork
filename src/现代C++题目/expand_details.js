f33b9a2ecc94208e3a598a9983fc3cf84dc5d1a5
rsp4jack <creepercdn@outlook.com>
2024-02-16 12:00:00 +0800

    Add expand_details

---
+++ src/现代C++题目/expand_details.js
@@ -0,0 +1,11 @@
+window,PagedConfig = window.PagedConfig || {};
+window.PagedConfig.before = window.PagedConfig.before || function() {};
+let originalBefore = window.PagedConfig.before;
+window.PagedConfig.before = () => {
+    // See https://github.com/pagedjs/pagedjs/issues/45
+    originalBefore.apply(this);
+
+    $('details').each((e) => {
+        $(e).attr('open', true);
+    });
+}
