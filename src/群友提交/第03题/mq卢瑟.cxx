26a3cd62714870431b010815e029c5a867b47508
LeeZQXML <2919625053@qq.com>
2024-02-29 13:17:23 +0800

    规范和更改md文件

--- src/群友提交/第03题/mq卢瑟.cxx
+++ src/群友提交/第03题/mq卢瑟.cxx
@@ -10,14 +10,12 @@ void print(std::string_view format_str, Args const&... args) {
     std::cout << std::vformat(format_str, std::make_format_args(std::forward<decltype(args)>(args)...));
 }
 
-namespace std {
-    template<>
-    struct formatter<Frac> : formatter<string> {
-        auto format(Frac const& f, format_context& ctx) const {
-            return format_to(ctx.out(), "{}/{}", f.a, f.b);
-        }
-    };
-}
+template<>
+struct std::formatter<Frac> : std::formatter<string> {
+    auto format(Frac const& f, format_context& ctx) const {
+        return std::format_to(ctx.out(), "{}/{}", f.a, f.b);
+    }
+};
 
 int main() {
     const Frac f{ 1, 10 };
