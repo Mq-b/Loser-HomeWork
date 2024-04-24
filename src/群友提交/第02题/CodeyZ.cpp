ecd1dcd172e72aa25708c17bf9297491cc612e01
CodeyZ <55646007+Codey-Z@users.noreply.github.com>
2024-04-24 08:48:14 +0800

    CodeyZ-first-pr (#315)
    * CodeyZ-first-pr
    
    1~3题

---
+++ src/群友提交/第02题/CodeyZ.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<format>
+#include<string.h>
+#include<numbers>
+
+struct display{
+private:
+    const char*  m_s;
+public:
+    display(const char* s) :m_s(s) {}
+
+    template<typename...Args>
+    auto operator()(Args...args) {
+        return std::vformat(m_s, std::make_format_args(std::forward<Args>(args)...));
+    }
+};
+
+auto operator""_f(const char* s,std::size_t n) {
+    return display{s};
+}
+
+int main() {
+    std::cout << "乐 :{} *\n"_f(5);
+    std::cout << "乐 :{0} {0} *\n"_f(5);
+    std::cout << "乐 :{:b} *\n"_f(0b01010101);
+    std::cout << "{:*<10}"_f("卢瑟");
+    std::cout << '\n';
+    int n{};
+    std::cin >> n;
+    std::cout << "π：{:.{}f}\n"_f(std::numbers::pi_v<double>, n);
+    return 0;
+}
---
+++ src/群友提交/第03题/CodeyZ.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<format>
+#include<string.h>
+#include<string_view>
+struct Frac {
+	int a, b;
+};
+
+template<>
+struct std::formatter<Frac, char> {
+
+	template<class ParseContext>
+	constexpr auto parse(ParseContext& ctx) {
+		auto it = ctx.begin();
+		if (it == ctx.end())
+			return it;
+		if (it != ctx.end() && *it != '}')
+			throw std::format_error("无效的 QuotableString 格式参数。");
+		return it;
+	}
+	template<class Fmtcontext>
+	auto format(::Frac f, Fmtcontext& ctx)const {
+		return format_to(ctx.out(), "{}/{}", f.a, f.b);
+	}
+};
+
+template<typename...Args>
+auto print(std::string_view s,Args&&...args) {
+	std::cout << std::vformat(s,std::make_format_args(std::forward<Args>(args)...)) << '\n';
+}
+
+int main() {
+	Frac f{ 1,10 };
+	print("{}", f);// 结果为1/10
+	//std::cout << std::format("{}", f);
+}
