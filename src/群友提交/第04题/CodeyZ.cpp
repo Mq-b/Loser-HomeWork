2a6cad02bdf93e578df3976c83dd1eeb84da7a01
CodeyZ <iamzgk19@163.com>
2024-04-24 11:58:47 +0800

    CodeyZ-sec-loserhomework

---
+++ src/群友提交/第04题/CodeyZ.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+class ComponentBase {
+protected:
+    static inline std::size_t component_type_count = 0;
+};
+template<typename T>
+class Component : public ComponentBase {
+public:
+    static std::size_t component_type_id() {
+        static std::size_t id = component_type_count++;
+        return id;
+    }
+   
+};
+class A : public Component<A>
+{};
+class B : public Component<B>
+{};
+class C : public Component<C>
+{};
+int main()
+{
+    std::cout << A::component_type_id() << std::endl;
+    std::cout << B::component_type_id() << std::endl;
+    std::cout << B::component_type_id() << std::endl;
+    std::cout << A::component_type_id() << std::endl;
+    std::cout << A::component_type_id() << std::endl;
+    std::cout << C::component_type_id() << std::endl;
+}
