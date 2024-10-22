5780413d2381fea7e50356ec024db28da8fb713b
Matrix-A <51079060+Matrix-A@users.noreply.github.com>
2024-10-22 09:18:37 +0800

    第十四题 使用 asm 指定符号名
    解释 asm("..")可以手动指定符号名。
    使用```std::atomic_thread_fence(std::memory_order_acq_rel);```是为了禁止将读取```ss::a```优化到写入 b 之前
    
    https://gcc.godbolt.org/z/WYqcc56r7

---
+++ src/群友提交/第14题/Matrix-A-3.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<atomic>
+
+namespace ss {
+    int a = 0;
+}
+
+int main() {
+    extern int b asm("_ZN2ss1aE");
+    b = 100;
+    std::atomic_thread_fence(std::memory_order_acq_rel);
+    std::cout << ss::a << '\n'; 
+}
