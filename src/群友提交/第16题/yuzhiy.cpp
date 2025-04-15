b30337b627d7dcee0b2d78c4433d8897446a128e
Yuzhiy05 <ImoutoCon1999@outlook.com>
2025-04-15 10:17:25 +0800

    Revert "patch1第二个提交"
    This reverts commit a074da91a5eb6fb3a4e29e4c49153eb60166eb9b.

--- src/群友提交/第16题/yuzhiy.cpp
+++ src/群友提交/第16题/yuzhiy.cpp
@@ -5,9 +5,3 @@
 {                       \
     return func(std::forward<decltype(args)>(args)...); \
 }
-
-
-template<typename F, class...Args>
-auto foo(F f, Args&&...args) {
-    return f(std::forward<Args>(args)...);
-}
